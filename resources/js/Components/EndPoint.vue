732657712c2aaaa630d7ac84f16d3acee0078f3d
wali-eldin-hassan <creaspo6@gmail.com>
2023-08-24 15:01:38 +0300

    toggling edit button

--- resources/js/Components/EndPoint.vue
+++ resources/js/Components/EndPoint.vue
@@ -2,46 +2,70 @@
 import { router } from '@inertiajs/vue3';
 import DangerButton from '@/Components/DangerButton.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import InputError from '@/Components/InputError.vue';
+import { ref } from 'vue';
+import { usePage } from '@inertiajs/vue3';
 
-
- defineProps({
-    endpoint: Object
+defineProps({
+    endpoint: Object,
+    endpointsFrequencies: Object
 });
 
 let deleteEndPoint = (id) => {
-    if(confirm("Are you sure you want to delete this endpoint"))
-    {
+    if (confirm("Are you sure you want to delete this endpoint")) {
         router.delete(`/endpoints/${id}`);
-
     }
-
-    
 }
+let page = usePage();
+
+const editing = ref(false);
 
 </script>
 
 <template >
     <tr>
         <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
-            <div>
-                <h2 class="font-medium text-gray-800 dark:text-white ">
-                    {{ endpoint.location }}
+            <template v-if="editing">
+                <InputLabel for="location" value="Location" class="sr-only" />
+                <TextInput id="location" type="text" class="block w-full h-9 text-sm" placeholder="e.g/pricing" />
+                <InputError class="mt-2" />
+            </template>
+            <template v-else>
+                <div>
+                    <h2 class="font-medium text-gray-800 dark:text-white ">
+                        {{ endpoint.location }}
 
-                  
-                </h2>
-                <p class="text-sm font-normal text-gray-600 dark:text-gray-400">
-                </p>
-            </div>
+                    </h2>
+                    <p class="text-sm font-normal text-gray-600 dark:text-gray-400">
+                    </p>
+                </div>
+            </template>
+
         </td>
 
         <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
-            <div>
-                <h2 class="font-medium text-gray-800 dark:text-white">
-                    {{ endpoint.frequency_lable }}
-                </h2>
-                <p class="text-sm font-normal text-gray-600 dark:text-gray-400">
-                </p>
-            </div>
+            <template v-if="editing">
+                <InputLabel for="frequency" value="Frequency" class="sr-only" />
+                <select name="frequency" id="frequency"
+                    class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md h-9 leading-none text-sm">
+                    <option :value="frequency.frequency" v-for="frequency in endpointsFrequencies.data"
+                        :key="frequency.frequency">
+                        {{ frequency.label }}
+                    </option>
+                </select>
+            </template>
+            <template v-else>
+                <div>
+                    <h2 class="font-medium text-gray-800 dark:text-white">
+                        {{ endpoint.frequency_lable }}
+                    </h2>
+                    <p class="text-sm font-normal text-gray-600 dark:text-gray-400">
+                    </p>
+                </div>
+            </template>
+
         </td>
 
         <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
@@ -73,18 +97,18 @@ let deleteEndPoint = (id) => {
                 </p>
             </div>
         </td>
-        <tr>
-            <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
-            <PrimaryButton >Edit</PrimaryButton>
+    <tr>
+        <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
+            <PrimaryButton @click="editing = !editing">{{ editing ? `Done` : `Edit` }}</PrimaryButton>
 
         </td>
 
         <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
 
-            <DangerButton type="submit" @click="deleteEndPoint(endpoint.id)"  >Delete</DangerButton>
+            <DangerButton type="submit" @click="deleteEndPoint(endpoint.id)">Delete</DangerButton>
 
         </td>
-        </tr>
-    
+    </tr>
+
     </tr>
 </template>
