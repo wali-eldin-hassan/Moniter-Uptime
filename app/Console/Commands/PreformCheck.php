dc5cfd0aa50ed24f734fa1a3d16873b68bf9f548
wali-eldin-hassan <creaspo6@gmail.com>
2023-08-25 16:14:16 +0300

    added command from preform checks

---
+++ app/Console/Commands/PreformCheck.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\EndPoint;
+use GrahamCampbell\ResultType\Success;
+use Illuminate\Console\Command;
+
+class PreformCheck extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'checks:preform';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'preform endpoint checks';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $endpoints = EndPoint::where('next_check', '<=', now())->each(function(){
+            
+            // jobs
+        });
+
+
+        return Command::SUCCESS;
+    }
+}
