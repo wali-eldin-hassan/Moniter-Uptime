6fd2ac59e6946ee4a4b2a89b0cf8459d4b93b75d
wali-eldin-hassan <creaspo6@gmail.com>
2023-08-25 18:00:11 +0300

    added check model and relationships with Endpoint model

---
+++ app/Models/Check.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Check extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['response_code', 'response_body'];
+
+    function endponits(): BelongsTo
+    {
+        return $this->belongsTo(EndPoint::class, 'end_point_id');
+    }
+}
