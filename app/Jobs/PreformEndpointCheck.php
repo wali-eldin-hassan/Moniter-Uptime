65022a62d0a574fccfc4c018a5a31bb133175267
wali-eldin-hassan <creaspo6@gmail.com>
2023-08-28 12:13:37 +0300

    added queue to project

--- app/Jobs/PreformEndpointCheck.php
+++ app/Jobs/PreformEndpointCheck.php
@@ -13,7 +13,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
-class PreformEndpointCheck
+class PreformEndpointCheck implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
